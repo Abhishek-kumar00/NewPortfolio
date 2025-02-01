b875d5243abf269b654d4fe51d7331931b088bec
Abhishek-kumar00 <abhpinran98@gmail.com>
2025-02-01 16:45:43 +0530

    Update image paths to use root-relative URLs and adjust fade-in transition duration

--- src/components/Contact/ContactIcons.jsx
+++ src/components/Contact/ContactIcons.jsx
@@ -6,7 +6,7 @@ function ContactIcons() {
   return (
     <div className='flex items-center mt-8 text-white'>
         <ul className='flex space-x-4 w-full justify-evenly items-center'>
-            <li className='github hover:scale-125 lg:w-20 hover:text-blue-400 w-15 sm:text-blue-400 '>
+            <li className='github hover:scale-125 lg:w-20 hover:text-blue-400 w-15  '>
                 <a className='' href="https://www.linkedin.com/in/abhishek-kumar-808167258/">
                    <FontAwesomeIcon icon={faLinkedin} size="2xl"  />
                 </a>
@@ -17,13 +17,13 @@ function ContactIcons() {
 
                 </a>
             </li>
-            <li className='github hover:scale-125 lg:w-20 hover:text-gray-700 w-15 sm:text-gray-700'>
+            <li className='github hover:scale-125 lg:w-20 hover:text-gray-700 w-15'>
                 <a className=' hover:scale-125 animate-bounce' href="https://x.com/abhpinran98">
                     <FontAwesomeIcon icon={faXTwitter} size="2xl" />
 
                 </a>
             </li>
-            <li className='github hover:scale-125 lg:w-20 hover:text-red-400 w-15 sm:text-red-400'>
+            <li className='github hover:scale-125 lg:w-20 hover:text-red-400 w-15 '>
                 <a className=' hover:scale-125 animate-bounce' href="https://github.com/Abhishek-kumar00">
                     <FontAwesomeIcon icon={faInstagram} size="2xl" />
 
